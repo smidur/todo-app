32e6831da0b9c91551e910cadc798e0162fb7f3a
smidr <vromabz@gmail.com>
2023-06-27 06:38:05 +0300

    complete and exit buttons implementation

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("To-Do",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Ubuntu', 12))
 while True:
     event, values = window.read()
@@ -22,7 +27,6 @@ while True:
         todos.append(new_todo)
         functions.write_todos(todos)
         window['todos'].update(values=functions.get_todos())
-
     elif event == "Edit":
         todo_to_edit = values['todos'][0]
         new_todo = values['todo']
@@ -32,9 +36,17 @@ while True:
         todos[index] = new_todo
         functions.write_todos(todos)
         window['todos'].update(values=functions.get_todos())
-
     elif event == "todos":
         window['todo'].update(value=values['todos'][0])
+    elif event == "Complete":
+        todo_to_complete = values['todos'][0]
+        todos = functions.get_todos()
+        todos.remove(todo_to_complete)
+        functions.write_todos(todos)
+        window['todos'].update(values=functions.get_todos())
+        window['todo'].update(value="")
+    elif event == "Exit":
+        break
 
     elif event == sg.WINDOW_CLOSED:
         break
