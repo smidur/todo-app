4f35bfa561730c74b138476ba756ce04ea0715db
smidr <vromabz@gmail.com>
2023-07-04 06:35:29 +0300

    added complete button; small bug fixes;

--- cli.py
+++ cli.py
@@ -7,60 +7,41 @@ print("It is:", now)
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action.strip()
-
     if user_action.startswith("show"):
         todos = functions.get_todos()
-
         for index, item in enumerate(todos):
             row = f"{index + 1}: {item.title()}"
             print(row)
-
     elif user_action.startswith("add"):
         todo = user_action[4:]
-
         todos = functions.get_todos()
-
         todos.append(todo)
-
         functions.write_todos(todos)
-
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5:])
             number -= 1
-
             todos = functions.get_todos()
-
             change = input(f"Type a new todo instead of '{todos[number]}': ")
             todos[number] = change
-
             functions.write_todos(todos)
         except ValueError:
             print("Invalid command.")
             continue
-
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-
             todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip()
             todos.pop(index)
-
             functions.write_todos(todos)
-
             message = f"Todo '{todo_to_remove}' was removed from the list."
             print(message)
         except IndexError:
             print("There's no item with that number.")
             continue
-
     elif user_action == "exit":
         break
     else:
         print("Invalid command.")
-
-
-
-
